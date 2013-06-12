41da021b27b626ef510364a0ee5fbce29d27af1a
Andrew Walker <andy@madebymany.co.uk>
2013-06-12 12:35:25 +0100

    updating dependencies and version bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
     meta: {
-      version: '0.3.0',
+      version: '0.3.1',
       banner: '// Big Bird\n// v<%= meta.version %>\n// by @cjbell88, @ninjabiscuit & @callumj_ all from @madebymany'
     },
 
