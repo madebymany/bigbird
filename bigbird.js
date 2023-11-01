40c251432cfe2ad2f67772f6c5f04c711f68d79c
Oli Matthews <oli@madebymany.co.uk>
2023-11-01 13:34:04 +0000

    prepar 0.3.6.2

--- bigbird.js
+++ bigbird.js
@@ -1,23 +1,24 @@
-(function(root, factory) {
+/** @format */
+
+(function (root, factory) {
   if (typeof define === "function" && define.amd) {
     define(["underscore", "eventablejs", "jquery"], factory);
   } else if (typeof exports === "object") {
     module.exports = factory(
       require("underscore"),
       require("eventablejs"),
-      require("jquery"));
+      require("jquery")
+    );
   } else {
     root.BigBird = factory(root._, root.Eventable, root.$);
   }
-}(this, function(_, Eventable, $) {
-
+})(this, function (_, Eventable, $) {
   "use strict";
 
   var BigBird = {};
 
-  BigBird.VERSION = "0.3.6.1";
+  BigBird.VERSION = "0.3.6.2";
   BigBird.Events = _.extend({}, Eventable);
-
 
   // Helpers
   // -------
@@ -29,12 +30,16 @@
     if (protoProps && protoProps.hasOwnProperty("constructor")) {
       child = protoProps.constructor;
     } else {
-      child = function() { parent.apply(this, arguments); };
+      child = function () {
+        parent.apply(this, arguments);
+      };
     }
 
     _.extend(child, parent, staticProps);
 
-    var Surrogate = function() { this.constructor = child; };
+    var Surrogate = function () {
+      this.constructor = child;
+    };
     Surrogate.prototype = parent.prototype;
     child.prototype = new Surrogate();
 
@@ -51,7 +56,7 @@
     var match = evt.match(/^(\S+)\s*(.*)$/);
     return {
       kind: match[1],
-      selector: match[2]
+      selector: match[2],
     };
   }
 
@@ -59,31 +64,33 @@
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
 
-
   // Initializer
   // -----------
 
-  var Initializer = BigBird.Initializer = function(options) {
+  var Initializer = (BigBird.Initializer = function (options) {
     var $base = options.base || $(document.body);
 
-    _.extend(this, {
-      modules: {},
-      module: $base.data("module"),
-      action: $base.data("action")
-    }, options);
+    _.extend(
+      this,
+      {
+        modules: {},
+        module: $base.data("module"),
+        action: $base.data("action"),
+      },
+      options
+    );
 
     this.initialize();
-  };
+  });
 
   _.extend(Initializer.prototype, {
-
-    initialize: function() {
+    initialize: function () {
       this.execute("common", "initialize");
       this.execute(this.module, "initialize");
       this.execute(this.module, this.action);
     },
 
-    execute: function(moduleName, actionName) {
+    execute: function (moduleName, actionName) {
       var module;
       var action;
 
@@ -91,119 +98,133 @@
         return;
       }
 
-      module = this.modules[moduleName] ||
-               this.modules[capitalise(moduleName)];
+      module = this.modules[moduleName] || this.modules[capitalise(moduleName)];
       if (_.isUndefined(module)) {
         return;
       }
 
-      action = module[actionName] ||
-               module[capitalise(actionName)];
+      action = module[actionName] || module[capitalise(actionName)];
       if (_.isFunction(action)) {
         action();
       }
     },
 
-    rerunAction: function() {
+    rerunAction: function () {
       this.execute(this.module, this.action);
-    }
-
+    },
   });
-
 
   // Module
   // ------
 
-  var Module = BigBird.Module = function(options) {
+  var Module = (BigBird.Module = function (options) {
     _.extend(this, options);
 
-    if (this.el) { this.setElement(); }
-    if (this.proxied) { this.proxyMethods(); }
-    if (this.events) { this.attachEvents(); }
-    if (this.subscriptions) { this.subscribeToEvents(); }
+    if (this.el) {
+      this.setElement();
+    }
+    if (this.proxied) {
+      this.proxyMethods();
+    }
+    if (this.events) {
+      this.attachEvents();
+    }
+    if (this.subscriptions) {
+      this.subscribeToEvents();
+    }
 
     return this.initialize.apply(this, arguments);
-  };
+  });
 
   Module.extend = extend;
 
   _.extend(Module.prototype, BigBird.Events, {
-
     publish: _.bind(BigBird.Events.trigger, BigBird.Events),
     subscribe: _.bind(BigBird.Events.on, BigBird.Events),
 
     $el: null,
     _$els: {},
 
-    initialize: function() {
-    },
+    initialize: function () {},
 
-    proxyMethods: function() {
+    proxyMethods: function () {
       _.bindAll.apply(null, _.union(this, this.proxied));
     },
 
-    attachEvents: function() {
+    attachEvents: function () {
       var method;
       var evt;
 
-      _.each(this.events, function(m, e) {
-        method = _.bind(this[m], this);
-        evt = splitEvent(e);
+      _.each(
+        this.events,
+        function (m, e) {
+          method = _.bind(this[m], this);
+          evt = splitEvent(e);
 
-        if (evt.selector) {
-          this.$el.on(evt.kind, evt.selector, method);
-        } else {
-          this.$el.on(evt.kind, method);
-        }
-      }, this);
+          if (evt.selector) {
+            this.$el.on(evt.kind, evt.selector, method);
+          } else {
+            this.$el.on(evt.kind, method);
+          }
+        },
+        this
+      );
     },
 
-    subscribeToEvents: function() {
-      _.each(this.subscriptions, function(method, name) {
-        this.subscribe(name, this[method], this);
-      }, this);
+    subscribeToEvents: function () {
+      _.each(
+        this.subscriptions,
+        function (method, name) {
+          this.subscribe(name, this[method], this);
+        },
+        this
+      );
     },
 
-    setElement: function(el) {
+    setElement: function (el) {
       var element = el || this.el;
       this.$el = element instanceof $ ? element : $(element);
       this.el = this.$el[0];
       this.data = this.$el.data();
     },
 
-    setElements: function() {
+    setElements: function () {
       _.each(this.$("[data-bb-el]"), this._setBBElement, this);
     },
 
-    els: function(name, force) {
+    els: function (name, force) {
       return this._getBBElement(name, !!force)[0];
     },
 
-    $els: function(name, force) {
+    $els: function (name, force) {
       return this._getBBElement(name, !!force);
     },
 
-    $: function(selector) {
+    $: function (selector) {
       return this.$el.find(selector);
     },
 
-    destroy: function() {
+    destroy: function () {
       var evt;
 
-      _.each(this.events, function(m, e) {
-        evt = splitEvent(e);
+      _.each(
+        this.events,
+        function (m, e) {
+          evt = splitEvent(e);
 
-        if (evt.selector) {
-          this.$el.off(evt.kind, evt.selector);
-        } else {
-          this.$el.off(evt.kind);
-        }
-      }, this);
+          if (evt.selector) {
+            this.$el.off(evt.kind, evt.selector);
+          } else {
+            this.$el.off(evt.kind);
+          }
+        },
+        this
+      );
 
       this.$el.remove();
     },
 
-    _getBBElement: function(name, force) {
+    _getBBElement: function (name, force) {
       var element = this._$els[name];
 
       if (force || _.isUndefined(element)) {
@@ -214,13 +235,11 @@
       return element;
     },
 
-    _setBBElement: function(el) {
+    _setBBElement: function (el) {
       var $el = $(el);
       this._$els[$el.data("bbEl")] = $el;
-    }
-
+    },
   });
 
   return BigBird;
-
-}));
+});
